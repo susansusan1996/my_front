6f4de1dd0dc6c68c32219af5de4563cb56764f3f
susansusan1996 <susansusan1996@gmail.com>
2025-02-24 15:40:08 +0800

    add github pages 路徑

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-// vite.config.js/ts
-import {defineConfig} from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
-import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
+import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 export default defineConfig({
+  base: '/my_front/', //github pages 路徑
   plugins: [
     vue(),
     Components({
