f955c9159161dbce56b0fc71e9e27229870c8a63
susansusan1996 <susansusan1996@gmail.com>
2025-02-24 16:54:44 +0800

    修正首頁

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,25 @@
 <template>
-  <BNavbar v-b-color-mode="'dark'" toggleable="lg" variant="dark" class="my-navbar">
-    <BNavbarBrand to="/">凰喜的作品集</BNavbarBrand>
+  <BNavbar
+    v-b-color-mode="'dark'"
+    toggleable="lg"
+    variant="dark"
+    class="my-navbar"
+  >
+    <BNavbarBrand to="/my_front/resume">凰喜的作品集</BNavbarBrand>
     <BNavbarToggle target="nav-collapse" />
     <BCollapse id="nav-collapse" is-nav>
       <BNavbarNav>
-        <BNavItem ></BNavItem>
+        <BNavItem to="/my_front/resume" @click="refresh">關於我</BNavItem>
       </BNavbarNav>
       <BNavbarNav class="ms-auto mb-2 mb-lg-0">
-        <BNavItemDropdown text="關於我" right>
-          <BDropdownItem to="/resume">自我介紹</BDropdownItem>
-        </BNavItemDropdown>
         <BNavItemDropdown text="遊戲" right>
-          <BDropdownItem to="/snake">貪吃蛇</BDropdownItem>
-          <BDropdownItem to="/mario">馬力歐</BDropdownItem>
-          <BDropdownItem to="/shoot">射擊遊戲</BDropdownItem>
+          <BDropdownItem to="/my_front/snake">貪吃蛇</BDropdownItem>
+          <BDropdownItem to="/my_front/mario">馬力歐</BDropdownItem>
         </BNavItemDropdown>
         <BNavItemDropdown text="資料結構學習" right>
-          <BDropdownItem to="/stack">stack</BDropdownItem>
-          <BDropdownItem to="/queue">queue</BDropdownItem>
-          <BDropdownItem to="/bst">binary search tree</BDropdownItem>
+          <BDropdownItem to="/my_front/stack">stack</BDropdownItem>
+          <BDropdownItem to="/my_front/queue">queue</BDropdownItem>
         </BNavItemDropdown>
-<!--        <BNavItemDropdown disabled="" text="離散數學" right>-->
-<!--        </BNavItemDropdown>-->
-<!--        <BNavItemDropdown disabled="" text="演算法" right>-->
-<!--        </BNavItemDropdown>-->
-<!--        <BNavItemDropdown disabled="" text="作業系統" right>-->
-<!--        </BNavItemDropdown>-->
-<!--        <BNavItemDropdown disabled="" text="計算機組織" right>-->
-<!--        </BNavItemDropdown>-->
       </BNavbarNav>
       <BNavForm class="d-flex">
         <BFormInput class="me-2" placeholder="Search" />
@@ -38,14 +30,19 @@
 </template>
 
 <script setup lang="ts">
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["refresh"]);
+
+function refresh() {
+  emit("refresh");
+}
 </script>
 
 <style scoped>
-
 .my-navbar {
   border-radius: 15px;
   margin: 10px;
   padding: 10px;
 }
-
 </style>
