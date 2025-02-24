51f62a6454151857f19aba6de7283169f265e7a0
susansusan1996 <susansusan1996@gmail.com>
2025-02-24 16:54:57 +0800

    修正footer

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,9 +1,7 @@
 <template>
   <footer id="footer" class="footer bg-light mt-4 p-1 small text-center">
-    版權所有 &copy; 中華民國 {{ currentMinguoYear }} 年
+    摳妮基挖~~ 歡迎光臨凰喜的小天地~
   </footer>
 </template>
 
-<script setup>
-    const currentMinguoYear = new Date().getFullYear() - 1911;
-</script>
+<script setup></script>
